1024de37d5060eb4d218a382bfe31b7b339d447c
brayanalexander <brayangenesis@gmail.com>
2022-10-31 20:40:08 -0500

    quitar cors

--- api/src/app.js
+++ api/src/app.js
@@ -3,7 +3,7 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const routes = require('./routes/index.js');
-const cors=require('cors');
+//const cors=require('cors');
 
 require('./db.js');
 
@@ -23,7 +23,7 @@ server.use((req, res, next) => {
   next();
 });
 
-server.use(cors());
+//server.use(cors());
 server.use(express.json());
 server.use('/', routes);
 
